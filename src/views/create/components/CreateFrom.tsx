d99481256e2c8c51b39cede764fecdb0f96f6dfb
Duosi-Dai <daiduosi1999@gmail.com>
2024-09-26 14:14:20 +0200

    create form style

--- src/views/create/components/CreateFrom.tsx
+++ src/views/create/components/CreateFrom.tsx
@@ -1,5 +1,6 @@
 import { useFormik } from "formik";
 import { useState, useRef } from "react";
+import "../../../styles/styles.scss";
 
 const CreateFrom = () => {
   const [imgURL, setImgURL] = useState("src/assets/placeholder/1.jpeg");
@@ -33,52 +34,55 @@ const CreateFrom = () => {
   });
 
   return (
-    <div>
-      <img src={imgURL} />
-      <button
-        className="bg-success border-0 text-white"
-        onClick={handleImageUpload}
-      >
-        Upload Image
-      </button>
-      <form onSubmit={formik.handleSubmit}>
-        <input
-          id="reviewPic"
-          name="reviewPic"
-          type="file"
-          ref={fileUploadedRef}
-          onChange={uploadImageDisplay}
-          value={formik.values.reviewPic}
-          hidden
-        />
-        <label htmlFor="reviewGame">Game</label>
-        <input
-          id="reviewGame"
-          name="reviewGame"
-          type="text"
-          placeholder="Game Name"
-          onChange={formik.handleChange}
-          value={formik.values.reviewGame}
-        />
-        <label htmlFor="reviewTilte">Title</label>
-        <input
-          id="reviewTitle"
-          name="reviewTitle"
-          type="text"
-          placeholder="Game Review Title"
-          onChange={formik.handleChange}
-          value={formik.values.reviewTitle}
-        />
-        <label htmlFor="reviewMessage">Review</label>
-        <textarea
-          id="reviewMessage"
-          name="reviewMessage"
-          placeholder="Review"
-          onChange={formik.handleChange}
-          value={formik.values.reviewMessage}
-        />
-        <button type="submit">Submit</button>
-      </form>
+    <div className="create-form">
+      <div className="create-form__image-upload">
+        <button
+          className="create-form__upload-button"
+          onClick={handleImageUpload}
+        >
+          <img src={imgURL} className="create-form__image" />
+        </button>
+      </div>
+      <div className="create-form__fields">
+        <form onSubmit={formik.handleSubmit} className="create-form__form">
+          <input
+            id="reviewPic"
+            name="reviewPic"
+            type="file"
+            ref={fileUploadedRef}
+            onChange={uploadImageDisplay}
+            value={formik.values.reviewPic}
+            hidden
+          />
+          <label htmlFor="reviewGame">Game</label>
+          <input
+            id="reviewGame"
+            name="reviewGame"
+            type="text"
+            placeholder="Game Name"
+            onChange={formik.handleChange}
+            value={formik.values.reviewGame}
+          />
+          <label htmlFor="reviewTilte">Title</label>
+          <input
+            id="reviewTitle"
+            name="reviewTitle"
+            type="text"
+            placeholder="Game Review Title"
+            onChange={formik.handleChange}
+            value={formik.values.reviewTitle}
+          />
+          <label htmlFor="reviewMessage">Review</label>
+          <textarea
+            id="reviewMessage"
+            name="reviewMessage"
+            placeholder="Review"
+            onChange={formik.handleChange}
+            value={formik.values.reviewMessage}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };

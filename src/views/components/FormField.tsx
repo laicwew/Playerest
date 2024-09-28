4a6c8b5ea1a2a852af386f707d921c3abf81cc16
Duosi-Dai <daiduosi1999@gmail.com>
2024-09-28 21:38:32 +0200

    Created reusable FormField component

---
+++ src/views/components/FormField.tsx
@@ -0,0 +1,53 @@
+interface FormFieldProps {
+  className: string;
+  id: string;
+  name: string;
+  type: string;
+  label: string;
+  value: string;
+  placeholder?: string;
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+}
+
+const FormField = ({
+  className,
+  id,
+  name,
+  type,
+  label,
+  value,
+  placeholder,
+  onChange,
+}: FormFieldProps) => {
+  return (
+    <div className={className}>
+      <label htmlFor={id} className={`${className}-label`}>
+        {label}
+      </label>
+      {type !== "textarea" ? (
+        <input
+          id={id}
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          onChange={onChange}
+          value={value}
+          className={`${className}-input`}
+        />
+      ) : (
+        <textarea
+          id={id}
+          name={name}
+          placeholder={placeholder}
+          onChange={onChange}
+          value={value}
+          className={`${className}-input`}
+        />
+      )}
+    </div>
+  );
+};
+
+export default FormField;
--- src/views/create/index.tsx
+++ src/views/create/index.tsx
@@ -1,7 +1,115 @@
-import CreateFrom from "./components/CreateFrom";
+import { useFormik } from "formik";
+import { useState, useRef } from "react";
+import "../../scss/styles.scss";
+import defaultImage from "../../assets/defaultImage.png";
+import FormField from "../components/FormField";
 
 export function Create() {
+  const [imgURL, setImgURL] = useState(defaultImage);
+  const fileUploadedRef = useRef<HTMLInputElement | null>(null);
+
+  const handleImageUpload = (event: React.MouseEvent) => {
+    event.preventDefault();
+    if (fileUploadedRef.current) {
+      fileUploadedRef.current.click();
+    }
+  };
+
+  const handleImageDelete = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setImgURL(defaultImage);
+  };
+
+  const uploadImageDisplay = () => {
+    const uploadedFile = fileUploadedRef.current?.files?.[0];
+    if (uploadedFile) {
+      const cachedURL = URL.createObjectURL(uploadedFile);
+      setImgURL(cachedURL);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      reviewTitle: "",
+      reviewText: "",
+      reviewGame: "",
+      reviewPic: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <CreateFrom />
+    <div className="create-form">
+      <div className="create-form__image-upload">
+        <img alt="create-img" src={imgURL} className="create-form__image" />
+        {imgURL === defaultImage ? (
+          <button
+            className="create-form__upload-button"
+            onClick={handleImageUpload}
+          >
+            <span className="fas fa-upload me-2" /> Upload
+          </button>
+        ) : (
+          <button
+            className="create-form__upload-button"
+            onClick={handleImageDelete}
+          >
+            <span className="fas fa-trash me-2" /> Delete
+          </button>
+        )}
+      </div>
+
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          id="reviewPic"
+          name="reviewPic"
+          type="file"
+          ref={fileUploadedRef}
+          onChange={uploadImageDisplay}
+          value={formik.values.reviewPic}
+          hidden
+        />
+        <div className="create-form__fields">
+          <FormField
+            className="create-form__fields--1"
+            id="reviewGame"
+            name="reviewGame"
+            type="input"
+            label="Game"
+            value={formik.values.reviewGame}
+            onChange={formik.handleChange}
+            placeholder="Game Name"
+          />
+          <FormField
+            className="create-form__fields--2"
+            id="reviewTitle"
+            name="reviewTitle"
+            type="input"
+            label="Title"
+            value={formik.values.reviewTitle}
+            onChange={formik.handleChange}
+            placeholder="Review Title"
+          />
+          <FormField
+            className="create-form__fields--3"
+            id="reviewText"
+            name="reviewText"
+            type="textarea"
+            label="Review"
+            value={formik.values.reviewText}
+            onChange={formik.handleChange}
+            placeholder="Review"
+          />
+        </div>
+        <button type="submit" className="create-form__submit-btn">
+          Publish
+        </button>
+        <button type="submit" className="create-form__save-btn">
+          Save
+        </button>
+      </form>
+    </div>
   );
 }

160a0d3351f9e63bcf56ca075e1e035b89c91108
AmiyaSX <2668590277@qq.com>
2024-09-28 16:09:08 +0200

    formatting

--- src/views/create/components/CreateFrom.tsx
+++ src/views/create/components/CreateFrom.tsx
@@ -40,99 +40,97 @@ const CreateFrom = () => {
   });
 
   return (
-    <>
-      <div className="create-form">
-        <div className="create-form__image-upload">
-          <img src={imgURL} className="create-form__image" />
-          {imgURL === defaultImage ? (
-            <button
-              className="create-form__upload-button"
-              onClick={handleImageUpload}
-            >
-              <span className="fas fa-upload" />
-              Upload
-            </button>
-          ) : (
-            <button
-              className="create-form__upload-button"
-              onClick={handleImageDelete}
-            >
-              <span className="fas fa-trash" /> Delete
-            </button>
-          )}
-        </div>
-
-        <form onSubmit={formik.handleSubmit}>
-          <input
-            id="reviewPic"
-            name="reviewPic"
-            type="file"
-            ref={fileUploadedRef}
-            onChange={uploadImageDisplay}
-            value={formik.values.reviewPic}
-            hidden
-          />
-          <div className="create-form__fields">
-            <div className="create-form__fields--1">
-              <label
-                htmlFor="reviewGame"
-                className="create-form__fields--1-label"
-              >
-                Game
-              </label>
-              <input
-                id="reviewGame"
-                name="reviewGame"
-                type="text"
-                placeholder="Game Name"
-                onChange={formik.handleChange}
-                value={formik.values.reviewGame}
-                className="create-form__fields--1-input"
-              />
-            </div>
-            <div className="create-form__fields--2">
-              <label
-                htmlFor="reviewTilte"
-                className="create-form__fields--2-label"
-              >
-                Title
-              </label>
-              <input
-                id="reviewTitle"
-                name="reviewTitle"
-                type="text"
-                placeholder="Game Review Title"
-                onChange={formik.handleChange}
-                value={formik.values.reviewTitle}
-                className="create-form__fields--2-input"
-              />
-            </div>
-            <div className="create-form__fields--3">
-              <label
-                htmlFor="reviewMessage"
-                className="create-form__fields--3-label"
-              >
-                Review
-              </label>
-              <textarea
-                id="reviewMessage"
-                name="reviewMessage"
-                placeholder="Review"
-                onChange={formik.handleChange}
-                value={formik.values.reviewMessage}
-                className="create-form__fields--3-input"
-              />
-            </div>
-          </div>
-          <button type="submit" className="create-form__submit-btn">
-            Publish
+    <div className="create-form">
+      <div className="create-form__image-upload">
+        <img src={imgURL} className="create-form__image" />
+        {imgURL === defaultImage ? (
+          <button
+            className="create-form__upload-button"
+            onClick={handleImageUpload}
+          >
+            <span className="fas fa-upload me-2" />
+            Upload
           </button>
-          <button type="submit" className="create-form__save-btn">
-            Save
+        ) : (
+          <button
+            className="create-form__upload-button"
+            onClick={handleImageDelete}
+          >
+            <span className="fas fa-trash" /> Delete
           </button>
-        </form>
+        )}
       </div>
-    </>
+
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          id="reviewPic"
+          name="reviewPic"
+          type="file"
+          ref={fileUploadedRef}
+          onChange={uploadImageDisplay}
+          value={formik.values.reviewPic}
+          hidden
+        />
+        <div className="create-form__fields">
+          <div className="create-form__fields--1">
+            <label
+              htmlFor="reviewGame"
+              className="create-form__fields--1-label"
+            >
+              Game
+            </label>
+            <input
+              id="reviewGame"
+              name="reviewGame"
+              type="text"
+              placeholder="Game Name"
+              onChange={formik.handleChange}
+              value={formik.values.reviewGame}
+              className="create-form__fields--1-input"
+            />
+          </div>
+          <div className="create-form__fields--2">
+            <label
+              htmlFor="reviewTilte"
+              className="create-form__fields--2-label"
+            >
+              Title
+            </label>
+            <input
+              id="reviewTitle"
+              name="reviewTitle"
+              type="text"
+              placeholder="Game Review Title"
+              onChange={formik.handleChange}
+              value={formik.values.reviewTitle}
+              className="create-form__fields--2-input"
+            />
+          </div>
+          <div className="create-form__fields--3">
+            <label
+              htmlFor="reviewMessage"
+              className="create-form__fields--3-label"
+            >
+              Review
+            </label>
+            <textarea
+              id="reviewMessage"
+              name="reviewMessage"
+              placeholder="Review"
+              onChange={formik.handleChange}
+              value={formik.values.reviewMessage}
+              className="create-form__fields--3-input"
+            />
+          </div>
+        </div>
+        <button type="submit" className="create-form__submit-btn">
+          Publish
+        </button>
+        <button type="submit" className="create-form__save-btn">
+          Save
+        </button>
+      </form>
+    </div>
   );
 };
 

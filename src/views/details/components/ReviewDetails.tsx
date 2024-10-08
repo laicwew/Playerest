efd01e2b958838a5283691777f4119fe0ce53a2b
AmiyaSX <2668590277@qq.com>
2024-10-08 04:48:20 +0200

    add basic view for detail view

--- src/views/details/components/ReviewDetails.tsx
+++ src/views/details/components/ReviewDetails.tsx
@@ -1,15 +1,23 @@
-import { Review } from "../../../helpers/hooks/api/useReview"
+import { Review } from "../../../helpers/hooks/api/useReview";
 
-export default function ReviewDetails({
-    review,
-    className
-}: {
-    review: Review
-    className?: string
-}) {
-    return (
-        <div className={`${className}`}>
-            {review.content}
-        </div>
-    )
+export default function ReviewDetails({ review }: { review: Review }) {
+  return (
+    <div className="row mt-4 align-items-center">
+     {/* Image on the left */}
+     <div className="col-md-4 text-md-start text-center">
+        <img
+          src={review.imageUrl}
+          alt={"review" + review.id}
+          className="rounded img-fluid"
+          style={{ maxWidth: "100%" }} // Ensures the image is responsive
+        />
+      </div>
+
+      {/* Text content on the right */}
+      <div className="col-md-8">
+        <h2>{review.title}</h2>
+        <p>{review.content}</p>
+      </div>
+    </div>
+  );
 }

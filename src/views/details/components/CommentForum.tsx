6a3ea2f9166c39bb0946fe7754669f0cfeb2cad9
Duosi-Dai <daiduosi1999@gmail.com>
2024-10-09 18:17:45 +0200

    comment forum send function added

--- src/views/details/components/CommentForum.tsx
+++ src/views/details/components/CommentForum.tsx
@@ -1,11 +1,34 @@
+import { useFormik } from "formik";
 import { Review } from "../../../helpers/hooks/api/useReview";
 import FormField from "../../components/FormField";
+import { useState } from "react";
 interface CommentForumProps {
   review: Review;
   setIsOpenComment: () => void;
 }
 
 const CommentForum = ({ review, setIsOpenComment }: CommentForumProps) => {
+  const [updatedReview, setUpdatedReview] = useState(review);
+  const formik = useFormik({
+    initialValues: {
+      comment: "",
+    },
+
+    onSubmit: (values) => {
+      const newComment = {
+        id: (updatedReview.comments?.length || 0) + 1,
+        user: "Anonymous",
+        content: values.comment,
+      };
+      const newComments = [...(updatedReview.comments || []), newComment];
+
+      setUpdatedReview((prevReview) => ({
+        ...prevReview,
+        comments: newComments,
+      }));
+      formik.resetForm();
+    },
+  });
   return (
     <div className="comment-forum">
       <div className="d-flex flex-row p-2 justify-content-around">
@@ -13,13 +36,13 @@ const CommentForum = ({ review, setIsOpenComment }: CommentForumProps) => {
         <button onClick={setIsOpenComment}>X</button>
       </div>
       <div className="comment-list">
-        {review.comments?.map((item) => (
+        {updatedReview.comments?.map((item) => (
           <div className="comment-card" key={item.id}>
             <div className="comment-card__person">
               <div className="comment-card__img-container">
                 <img
                   className="comment-card__img-container--img"
-                  src={review.imageUrl}
+                  src={updatedReview.imageUrl}
                   alt="profile"
                 />
               </div>
@@ -30,16 +53,20 @@ const CommentForum = ({ review, setIsOpenComment }: CommentForumProps) => {
           </div>
         ))}
       </div>
-      <div className="comment-send">
-        <FormField
-          className="comment-send"
-          id="1"
-          name="comment"
-          type="input"
-          label=" "
-        />
-        <button>send</button>
-      </div>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="comment-send">
+          <FormField
+            className="comment-send"
+            id="1"
+            name="comment"
+            type="input"
+            label=""
+            onChange={formik.handleChange}
+            value={formik.values.comment}
+          />
+          <button type="submit">send</button>
+        </div>
+      </form>
     </div>
   );
 };

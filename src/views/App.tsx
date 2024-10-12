97754c96667c332e0d5a97748d47e56d95fa7297
Duosi-Dai <daiduosi1999@gmail.com>
2024-10-12 13:28:59 +0200

    theme: dark mode added

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,17 +1,29 @@
 import { Outlet } from "react-router-dom";
 import { AppNavBar } from "./components/AppNavBar";
+import { useState } from "react";
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+
+  const handleChange = () => {
+    setIsDark((prev) => !prev);
+  };
+
   return (
     /** TODO: light and dark theme */
     <div
-      className="container-fluid light-theme"
+      className={`container-fluid ${isDark ? "dark" : "light"}-theme`}
       style={{
         height: "100vh",
         width: "100vw",
       }}
     >
       <AppNavBar />
+      <i
+        onClick={handleChange}
+        className={`fa fa-toggle-${isDark ? "on" : "off"}`}
+        style={{ fontSize: "2rem" }}
+      ></i>
       <main>
         <Outlet />
       </main>
---
+++ src/views/components/ToggleSwitch.tsx
@@ -0,0 +1,5 @@
+const ToggleSwitch = () => {
+  return <div className="toggle-container"></div>;
+};
+
+export default ToggleSwitch;

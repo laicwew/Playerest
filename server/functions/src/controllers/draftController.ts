3d411c2260e8fb559943434f173c6358caa6f6c7
yileicheng <yilei.cheng.cyl@gmail.com>
2024-10-13 16:07:49 +0200

    feat: add get drafts by username endpoint

--- server/functions/src/controllers/draftController.ts
+++ server/functions/src/controllers/draftController.ts
@@ -1,21 +1,25 @@
-import {Request, Response} from "express";
-import {storeDraft, publishDraft} from "../services/dynamoService";
+import { Request, Response } from "express";
+import {
+  storeDraft,
+  publishDraft,
+  getDraftsByUserName,
+} from "../services/dynamoService";
 
 export const storeDraftHandler = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  const {imageUrl, author, title, content, rate} = req.body;
+  const { imageUrl, author, title, content, rate } = req.body;
 
   if (!author || !title || !content) {
-    res.status(400).json({error: "author, title, and content are required"});
+    res.status(400).json({ error: "author, title, and content are required" });
   }
 
   try {
-    const result = await storeDraft({imageUrl, author, title, content, rate});
+    const result = await storeDraft({ imageUrl, author, title, content, rate });
     res.status(201).json(result);
   } catch (error) {
-    res.status(500).json({error: "Error storing draft"});
+    res.status(500).json({ error: "Error storing draft" });
   }
 };
 
@@ -23,16 +27,36 @@ export const publishDraftHandler = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  const {draftId} = req.body;
+  const { draftId } = req.body;
 
   if (!draftId) {
-    res.status(400).json({error: "draftId is required"});
+    res.status(400).json({ error: "draftId is required" });
   }
 
   try {
     const result = await publishDraft(draftId);
     res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({error: "Error publishing draft"});
+    res.status(500).json({ error: "Error publishing draft" });
+  }
+};
+
+export const getDraftsByUserNameHandler = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId } = req.params;
+
+  if (!userId) {
+    res.status(400).json({ error: "UserId is required" });
+  }
+
+  try {
+    const drafts = await getDraftsByUserName(userId);
+    res.status(200).json(drafts);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: `Error fetching drafts for userId ${userId}` });
   }
 };
--- server/functions/src/routes/draftRoutes.ts
+++ server/functions/src/routes/draftRoutes.ts
@@ -2,12 +2,13 @@ import express from "express";
 import {
   storeDraftHandler,
   publishDraftHandler,
+  getDraftsByUserNameHandler,
 } from "../controllers/draftController";
 
 const router = express();
 
 router.post("/store", storeDraftHandler);
-
 router.post("/publish", publishDraftHandler);
+router.get("/user/:userId", getDraftsByUserNameHandler);
 
 export default router;
